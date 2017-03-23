3ad051d61f578a111e1b885d7e1684f0ead5c626
Deep Mehta <deepmehta.it@live.com>
2017-03-23 14:25:49 -0700

    Installing redux, eslint setup.

---
+++ RNList/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+
+const RNList = () => (
+    <View>
+      <Text>{'sdfsdfs'}</Text>
+    </View>
+  );
+
+AppRegistry.registerComponent('RNList', () => RNList);
