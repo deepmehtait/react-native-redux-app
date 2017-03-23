3e5a87d04e9c6f9efb60a5e484a5370930b03f8d
Deep Mehta <deepmehta.it@live.com>
2017-03-23 14:53:29 -0700

    Setting up reducers.

--- RNList/src/index.js
+++ RNList/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { AppRegistry, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
 const RNList = () => (
-    <View>
-      <Text>{'sdfsdfs'}</Text>
-    </View>
+    <Provider store={createStore(reducers)} >
+      <View>
+        <Text>{'sdfsdfs'}</Text>
+      </View>
+    </Provider>
   );
 
 AppRegistry.registerComponent('RNList', () => RNList);
